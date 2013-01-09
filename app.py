78d466c44cc4fcf7af3580dc2a19470ecd988fd0
stephanec <schorlet@gmail.com>
2013-01-09 21:39:59 +0100

    1ere question

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
 from flask import Flask, request
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def answer():
-    q = request.args.get("q", "")
-    return "Flask"
 
-if __name__ == "__main__":
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 8080))
+@app.route('/')
+def answer():
+    user = { 'nickname': 'stephanec',
+        'mail': 'schorlet@gmail.com' }
+    q = request.args.get('q', '')
+
+    if q == 'Quelle est ton adresse email':
+        return user['mail']
+
+    return render_template('index.html', user=user)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     # app.run(host='127.0.0.1', port=port, debug=True)
