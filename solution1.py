75fc884193660d427642da9c824a60a1f01df46c
stephanec <schorlet@gmail.com>
2013-01-11 19:45:26 +0100

    1er exercice

--- solution1.py
+++ solution1.py
@@ -1,61 +1,46 @@
 #-*- coding:utf-8 -*-
 
-def solution(montant):
-    monnaie = { '1': 'foo', '7': 'bar', '11': 'qix', '21': 'baz' }
-    permutations = ((21,11,7,1), (21,11,1), (21,7,1), (21,1), (11,7,1), (11,1), (7,1), (1,0))
+# monnaie = { 1: '1', 7: '7', 11: '11', 21: '21' }
+monnaie = { 1: 'foo', 7: 'bar', 11: 'qix', 21: 'baz' }
+
+def solution(montant, pieces = None):
     compositions = []
 
-    if not montant or montant > 80:
+    if not montant:
         return compositions
 
-    for permutation in permutations:
-        change = montant
-        # print '--permutation', permutation
-        composition = {}
-        total = 0
+    if not pieces:
+        pieces = sorted(monnaie.keys(), reverse = True)
 
-        for piece in permutation:
-            if piece == 0:
-                break
+    for piece in pieces:
+        if piece > montant:
+            continue
 
-            if piece > change:
-                break
+        if piece == 1:
+            composition = {monnaie[piece]: montant}
+            compositions.append(composition)
+            break
 
-            nb_pieces, change = change // piece, change % piece
-            if nb_pieces:
-                composition[monnaie[str(piece)]] = nb_pieces
-                total += piece * nb_pieces
+        nb_pieces = montant // piece
+        for nb_piece in range(1, nb_pieces + 1):
+            reste = montant - (piece * nb_piece)
 
-            if piece > 1 and nb_pieces > 1:
-                for nb_piece in range(nb_pieces - 1, 0, -1):
-                    debut_composition = { monnaie[str(piece)]: nb_piece }
-                    fins_compositions = solution(change + total - (piece * nb_piece))
+            if reste:
+                suite_compositions = solution(reste, pieces[pieces.index(piece) + 1:])
 
-                    for fin_composition in fins_compositions:
-                        if monnaie[str(piece)] in fin_composition.keys():
-                            continue
-
-                        autres_composition = dict(debut_composition.items() + fin_composition.items())
-                        if not autres_composition in compositions:
-                            compositions.append(autres_composition)
-
-            if not change:
-                break
-
-        if total == montant:
-            if not composition in compositions:
+                for suite_composition in suite_compositions:
+                    composition = {monnaie[piece]: nb_piece}
+                    composition.update(suite_composition)
+                    compositions.append(composition)
+            else:
+                composition = {monnaie[piece]: nb_piece}
                 compositions.append(composition)
+                break
 
     return compositions
 
 
 if __name__ == "__main__":
-    # print solution(1)
-    # print solution(7)
-    # print solution(8)
-    # print solution(11)
-    # print solution(12)
-    print solution(19)
-    print solution(21)
-    print solution(22)
-    print solution(92)
+    for i in range(1, 101):
+        print solution(i)
+
